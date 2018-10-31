a04cc37906d513a7916256cde9401a349eee8a04
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-10-31 08:32:22 +0530

    many to one mapping project setup

---
+++ manytoone/src/main/java/com/manjula/hibernate/manytoone/App.java
@@ -0,0 +1,13 @@
+package com.manjula.hibernate.manytoone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
