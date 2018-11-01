7d0feabcf38b626da7a1c897dd3a8f7a10e84cb7
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-11-01 14:56:52 +0530

    one to one mapping initial commit

---
+++ onetoone/src/main/java/com/manjula/hibernate/onetoone/App.java
@@ -0,0 +1,13 @@
+package com.manjula.hibernate.onetoone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
